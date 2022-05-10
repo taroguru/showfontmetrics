64a48547ad60b0321ba75c8c14a8123d22edba95
taroguru <taroguru@gmail.com>
2022-05-10 18:32:15 +0900

    메인 텍스트 박스 별도로 뽑기

--- showfontmetrics/src/App.svelte
+++ showfontmetrics/src/App.svelte
@@ -1,25 +1,55 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
 <script lang="ts">
+	import StyledTextArea from "./StyledTextArea.svelte";
+	let styledTextArea;
 
 	let mainText: string;
 	mainText='Lorem Ipsum';
+	
+	let fontSize: number;
+	fontSize=16;
 
-	function InputHandler (event: Event) {
-		mainText = event.target.value;
+	function applyFontStyle(){
+		styledTextArea.applyFontStyleModule();
 	}
+	
 </script>
- 
+
 <main>
-	<h1>Show me the FontMetrics</h1>
-	<textarea  value={mainText} on:input={InputHandler} />
-	<div>Debug Console : <br />{mainText}</div>
+	<div class="mainForm">
+		<h1>Show me the FontMetrics</h1>
+		<StyledTextArea bind:mainText={mainText} fontSize={fontSize} bind:this={styledTextArea} />
+	</div>
+	<hr>
+	<div class="StyleInputs">
+		<p>Style Inputs</p>
+		<label for="inputFontSize">Font Size:</label>
+		<input id="inputFontSize" type="text" bind:value={fontSize} on:input={applyFontStyle} />
+<!--
+		<label for="inputFontURL">Font URL:</label>
+		<input id="inputFontURL" type="text" value={fontURL} on:change={e=>fontURL=e.target.value} />
+		-->
+	</div>
+	<hr>
+	<div class="DebugConsole">
+		<p>Debug Console</p>
+		<p>MainText : {mainText}</p>
+		<p>FontSize : {fontSize}</p>
+	</div>
 </main>
 
 <style>
+	.DebugConsole {
+		text-align: left;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
+		font-family: 'Roboto', sans-serif;
 	}
 
 	h1 {
@@ -28,10 +58,11 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
-
+	
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
+
 	}
 </style>
---
+++ showfontmetrics/src/StyledTextArea.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+     
+    export let mainText: string;
+    export let fontSize: number;
+    
+    let appliedStyle: string;
+
+	export function applyFontStyleModule(){
+		appliedStyle=`
+			font-size: ${fontSize}px;
+		`;
+	}
+
+</script>
+
+<textarea bind:value={mainText} style={appliedStyle} />
+
+<style>
+	textarea{
+		width: 80%;
+		height: 200px;
+	}
+</style>
